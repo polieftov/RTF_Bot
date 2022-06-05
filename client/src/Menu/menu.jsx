bb6cd521fe96c95f53d589cbbfc2dbb1153e250b
polieftov <polieftovegor@gmail.com>
2022-06-05 20:05:40 +0500

    тулбар с кнопкой выйти

--- client/src/Menu/menu.jsx
+++ client/src/Menu/menu.jsx
@@ -1,38 +1,42 @@
 import React from "react";
 import './menu.css'
-import { IconButton } from "@mui/material";
-import { Button } from "@mui/material";
-import { SvgIcon } from "@mui/material";
-import { MainPage } from "../MainPage";
 
-const tbItems = [{value: "Выход", icon: "logout"}]
+const tbItems = [{value: "Выход", icon: "logout", href: '/'}]
 const Menu = ({header, items}) => {
     return (
-        <div className={'menu active'}>
-            <div className="menu_content" onClick={e => e.stopPropagation()}>
-                <div className="menu_header">{header}</div>
-                <ul className="menu-list">
-                <img src="icon.svg" className="AppLogo"></img>
-                    {items.map(item =>
-                        <li className="side-menu-item">
-                            <a href={item.href}>{item.value}</a>
-                            <span class="material-symbols-rounded">{item.icon}</span>
-                        </li>
-                    )}
-                </ul>
-                <div className="ToolBar">
-                    <div class="mdc-touch-target-wrapper">
-                        <button id="logoutBtn">
+        <div>
+            <div className="ToolBar">
+                <div className="mdc-touch-target-wrapper">
+                    <div id="logoutBtn">
 
-                            {tbItems.map(item=>
-                                <ul className="toolBar-items-list">
-                                    <a href={item.href}>{item.value}</a>
-                                    <span class="material-symbols-rounded">{item.icon}</span>
-                                </ul>
-                            )}
+                        {tbItems.map(item =>
+                            <ul className="toolBar-items-list">
+                                <a href={item.href}>
+                                    <span
+                                        className="material-symbols-rounded-exit material-symbols-rounded">{item.icon}</span>
+                                    {item.value}</a>
+                            </ul>
+                        )}
 
-                        </button>
                     </div>
+                </div>
+            </div>
+            <div className={'menu active'}>
+                <div className="menu_content" onClick={e => e.stopPropagation()}>
+                    <div className="menu_header">
+                        <img src="icon.svg" className="AppLogo"/>
+                        {header}
+                    </div>
+                    <ul className="menu-list">
+                        {items.map(item =>
+                            <li className="side-menu-item">
+                                <a href={item.href}>{item.value}</a>
+                                <span
+                                    class="material-symbols-rounded material-symbols-rounded-menu-items">{item.icon}</span>
+                            </li>
+                        )}
+                    </ul>
+
                 </div>
             </div>
         </div>
